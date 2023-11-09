4ca7ea751e32db9203d54cf37c9f8f14d2fadadf
Linda Achieng <achielinda44@gmail.com>
2023-11-08 21:59:12 -0800

    Fix tests

--- test/flyster/goals_test.exs
+++ test/flyster/goals_test.exs
@@ -8,21 +8,21 @@ defmodule Flyster.GoalsTest do
     test "saves a goal given valid params" do
       user = user_fixture()
 
-      assert {:ok, goal} = Goals.save_goal(%{description: "Master Ayesha", accomplish_by: "2023-11-19", user_id: user.id})
+      assert {:ok, _goal} = Goals.save_goal(%{description: "Master Ayesha", accomplish_by: "2023-11-19", creator_id: user.id, private: false, category: "Pole"})
     end
 
     test "goal is not saved given invalid params" do
       user = user_fixture()
-      assert {:error, changeset} = Goals.save_goal(%{description: "Master Ayesha", accomplish_by: "", user_id: user.id})
+      assert {:error, _changeset} = Goals.save_goal(%{description: "Master Ayesha", accomplish_by: "", creator_id: user.id, private: false, category: "Silks"})
     end
   end
 
   describe "update_goal/2" do
     test "updates a goal" do
       user = user_fixture()
-      {:ok, goal} = Goals.save_goal(%{description: "Master Ayesha", accomplish_by: "2023-11-19", user_id: user.id})
+      {:ok, goal} = Goals.save_goal(%{description: "Master Ayesha", accomplish_by: "2023-11-19", creator_id: user.id, private: false, category: "Fire"})
 
-      assert {:ok, goal} = Goals.update_goal(goal, %{accomplished: true})
+      assert {:ok, _goal} = Goals.update_goal(goal, %{accomplished: true})
     end
 
   end
