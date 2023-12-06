04373ee963af3c93c062d5356896546a047d2096
Linda Achieng <achielinda44@gmail.com>
2023-12-05 21:03:00 -0800

    Style challenges (#43)

--- lib/flyster_web/live/challenges/index_live.ex
+++ lib/flyster_web/live/challenges/index_live.ex
@@ -3,23 +3,6 @@ defmodule FlysterWeb.ChallengesIndexLive do
 
   alias Flyster.Context.Challenges
 
-  def render(assigns) do
-    ~H"""
-      All Challenges
-
-      <div class="space-y-12 divide-y">
-        <%= for challenge <- @challenges do %>
-         <div class="grid gap-6">
-          <%= challenge.creator.username %>
-          <%= challenge.category %>
-          <%= challenge.level %>
-          <%= challenge.description %>
-         </div>
-        <% end %>
-      </div>
-    """
-  end
-
   def mount(_params, _session, socket) do
     current_user = socket.assigns.current_user
     challenges = Challenges.all_challenges()
