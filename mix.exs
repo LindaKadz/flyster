164d776737dbe08c1e591d906b62cf18cd2ebc3d
Linda Achieng <achielinda44@gmail.com>
2023-12-21 16:38:09 -0800

    Add extra application

--- mix.exs
+++ mix.exs
@@ -19,7 +19,7 @@ defmodule Flyster.MixProject do
   def application do
     [
       mod: {Flyster.Application, []},
-      extra_applications: [:logger, :runtime_tools]
+      extra_applications: [:logger, :runtime_tools, :ssl]
     ]
   end
 
